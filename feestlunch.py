f646a9a4ff65eeada243cc40d57e44b4f30259f9
Mijart <99072617@mydavinci.nl>
2022-09-08 12:17:13 +0200

    Update

---
+++ feestlunch.py
@@ -0,0 +1,13 @@
+aantal = int(17)
+prijs = float(0.39)
+croissantjes = aantal*prijs
+
+aantal = int(2)
+prijs = float(2.78)
+stokbroden = aantal*prijs
+
+aantal = int(3)
+prijs = float(0.50)
+kortingsbonnen = 3*0.50
+
+print(f'{croissantjes + stokbroden - kortingsbonnen}')
