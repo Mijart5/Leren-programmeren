97e0a85cf8367f63363cbe58f0d8e0f92d2c80b0
Mijart <99072617@mydavinci.nl>
2022-09-08 15:49:19 +0200

    update

--- Overig/pannekoekens.py
+++
@@ -1,9 +0,0 @@
-pannekoeken=int(input("pannekoeken: "))
-eieren=pannekoeken*0.15
-melk=pannekoeken*40
-zout=pannekoeken*0.05
-boter=pannekoeken*2.50
-
-
-
-calc_pannenkoeken=bloem=pannekoeken*25 eieren=pannekoeken*0.15 melk=pannekoeken*40 zout=pannekoeken*0.05 boter=pannekoeken*2.50
