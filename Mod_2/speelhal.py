b508227f40ed2226b511748d9bd84e038deda902
Mijart <99072617@mydavinci.nl>
2022-09-08 18:29:00 +0200

    Speelhal update

--- Mod_2/speelhal.py
+++ Mod_2/speelhal.py
@@ -1,5 +1,8 @@
-ticket = 7.45*4
-tijd = 9 #tijd is eigenlijk 45 minuten en in 45 minuten past 5 minuten 9 keer
-prijs = 0.37 * 4 #0.37 is de prijs per 5 minuten
+ticket = input(str('Hoeveel kost/en de ticket/s? '))
+tijd_VIP_VR = input(str('Hoelang wilt u VIP VR gebruiken? '))
+prijs_VIP_VR = input(str('Wat is de prijs van VIP VR? '))
+personen = input(str('Met hoeveel persoon/en bent u? '))
 
-print(f'totaal prijs = {ticket + tijd * prijs:.2f}')
+totaal_prijs = (float(ticket)) * (int(personen)) + (float(tijd_VIP_VR)) / 5 * (float(prijs_VIP_VR))
+
+print(f'totaal prijs = {totaal_prijs:.2f}')
