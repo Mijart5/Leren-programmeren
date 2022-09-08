b0195eaf5c437ae8563d9e0e32c11c178f4ecee3
Mijart <99072617@mydavinci.nl>
2022-09-08 17:57:10 +0200

    update

---
+++ Mod_2/Person.py
@@ -0,0 +1,6 @@
+naam = input(str("Wat is je naam? "))
+adres = input(str("Wat is je straatnaam en huisnummer? "))
+postcode = input(str("Wat is je postcode? "))
+woonplaats = input(str("Wat is je woonplaats? "))
+
+print(f'---------------------------------------\n-{naam}\n-{adres}\n-{postcode}\n-{woonplaats}\n---------------------------------------')
