955b0353335c99a539edf3538aa575b3e5dcf468
Mijart <99072617@mydavinci.nl>
2022-09-08 15:43:41 +0200

    update

--- Mod_2/feestlunch.py
+++ Mod_2/feestlunch.py
@@ -1,13 +1,13 @@
-aantal_croissantjes = (17)
-prijs_croissantjes = (0.39)
-croissantjes = aantal_croissantjes*prijs_croissantjes
+aantal_croissantjes = 17
+prijs_croissantjes = 0.39
+croissantjes = aantal_croissantjes * prijs_croissantjes
 
-aantal_stokbroden = (2)
-prijs_stokbroden = (2.78)
-stokbroden = aantal_stokbroden*prijs_stokbroden
+aantal_stokbroden = 2
+prijs_stokbroden = 2.78
+stokbroden = aantal_stokbroden * prijs_stokbroden
 
-aantal_kortingsbonnen = (3)
-afprijzing_kortingsbonnen = (0.50)
+aantal_kortingsbonnen = 3
+afprijzing_kortingsbonnen = 0.50
 kortingsbonnen = 3 * 0.50
 
 print(f'De totaal prijs van de feestlunch = {croissantjes + stokbroden - kortingsbonnen}')
