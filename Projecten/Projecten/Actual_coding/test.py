d7bd7a6a28a49700065dbced10babc49cbfe8219
Mijart <99072617@mydavinci.nl>
2022-09-08 11:40:39 +0200

    patch 3

---
+++ Projecten/Projecten/Actual_coding/test.py
@@ -0,0 +1,4 @@
+count = int(input("aantal: "))
+item = input("item: ")
+price = float(input("prijs: "))
+print(f'{count} {item}  {price} {count*price:.2f}')
---
+++ Projecten/Projecten/Cheat/Spiekbriefje.py
@@ -0,0 +1,15 @@
+# zo maak je commentaar
+
+#||||||||||||||||||||||||||||||||||||
+# dit zijn variablen
+#||||||||||||||||||||||||||||||||||||
+
+aantal = 3   # dit is een integer
+omschrijving = "Lays naturel 300gr" #dit is een string
+prijs = 0.89 #dit is een float
+
+#||||||||||||||||||||||||||||||||||||||
+# beeld in functies
+#||||||||||||||||||||||||||||||||||||||
+
+print(f'{aantal} {omschrijving} {prijs} totaal prijs = {aantal*prijs:.2f}')
