b0f8ed0e90ccbf8141791b3165ac884762b7b456
Mijart <99072617@mydavinci.nl>
2022-09-09 11:41:30 +0200

    Alles van meten is weten in 1

---
+++ Mod_2/Statements_first_encounter.py
@@ -0,0 +1,11 @@
+a = int(input('geef een geheel getal: '))
+
+b = int(input('geef een geheel getal: '))
+
+if a == b:
+    print('De getallen zijn gelijk')
+else: print('De getallen zijn niet gelijk')
+
+if a > b:
+    print(f'{a} is groter dan {b}')
+else: print(f'{b} is groter dan {a}')
