bfc0dd8548c3fd1e389b1f5e5ae00dc9d882b76c
Mijart <99072617@mydavinci.nl>
2022-09-12 10:26:56 +0200

    solicitatie klaar

--- Mod_2/solicitatie.py
+++ Mod_2/solicitatie.py
@@ -1,16 +1,33 @@
-dressuur = input('Hoeveel jaar ervaring heeft u met dieren-dressuur? ')
-jongleren = input('hoeveel jaar ervaring heeft u met jongleren? ')
-acrobatiek = input('hoeveel jaar ervaring heeft u met acrobatiek? ')
+dressuur = int(input('Hoeveel jaar ervaring heeft u met dieren-dressuur? '))
+jongleren = int(input('hoeveel jaar ervaring heeft u met jongleren? '))
+acrobatiek = int(input('hoeveel jaar ervaring heeft u met acrobatiek? '))
 diploma = input('Welk MBO diploma heeft u? ')
 rijbewijs = input('bent u in bezit van een geldig Vrachtwagen rijbewijs? ')
 hoed = input('bent u in bezit van een hoge hoed? ')
 man = input('bent u een man EN heeft u Snor breder dan 10 cm? ')
 vrouw = input('bent u een vrouw met rood krulhaar langer dan 20cm? ')
-lengte = input('Wat is uw lengte in centimeters? ')
-gewicht = input('Wat is uw gewicht in kilogram? ')
+lengte = int(input('Wat is uw lengte in centimeters? '))
+gewicht = int(input('Wat is uw gewicht in kilogram? '))
 certificaat = input('Heeft u certificaat “Overleven met gevaarlijk personeel”? ')
 
-if dressuur == '4' or jongleren =='3' or acrobatiek == '3' and diploma == 'MBO-4 ondernemen' and rijbewijs == 'ja' and hoed == 'ja' and man == 'ja' or vrouw == 'ja' and lengte > 150 and gewicht > 90 and certificaat == 'ja':
+
+phase1 = False
+
+if dressuur >= 4 or jongleren >= 3 or acrobatiek >= 3:
+    phase1 = True
+
+phase2 = False
+
+if phase1 == True and diploma =='MBO-4 ondernemen' and rijbewijs == 'ja' and hoed == 'ja':
+    phase2 = True
+
+phase3 = False
+
+if man == 'ja' or vrouw == 'ja':
+    phase3 = True
+
+if (lengte > 150 and gewicht > 90 and certificaat == 'ja'
+    and phase1 == True and phase2 == True and phase3 == True):
     print('U voldoet aan de eisen')
 else:
-    print('U voldoet niet aan de eisen')
+    print('u voldoet niet aan de eisen')
