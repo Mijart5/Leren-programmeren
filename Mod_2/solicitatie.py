dc61cf8fda7a37d8f7f5ceb38e2e8f501d0e2776
Mijart <99072617@mydavinci.nl>
2022-09-12 09:37:57 +0200

    solicitatie klaar

--- Mod_2/solicitatie.py
+++ Mod_2/solicitatie.py
@@ -1,6 +1,16 @@
-poep = input('heb je gepoept? ')
-piss = input('heb je geplast? ')
+dressuur = input('Hoeveel jaar ervaring heeft u met dieren-dressuur? ')
+jongleren = input('hoeveel jaar ervaring heeft u met jongleren? ')
+acrobatiek = input('hoeveel jaar ervaring heeft u met acrobatiek? ')
+diploma = input('Welk MBO diploma heeft u? ')
+rijbewijs = input('bent u in bezit van een geldig Vrachtwagen rijbewijs? ')
+hoed = input('bent u in bezit van een hoge hoed? ')
+man = input('bent u een man EN heeft u Snor breder dan 10 cm? ')
+vrouw = input('bent u een vrouw met rood krulhaar langer dan 20cm? ')
+lengte = input('Wat is uw lengte in centimeters? ')
+gewicht = input('Wat is uw gewicht in kilogram? ')
+certificaat = input('Heeft u certificaat “Overleven met gevaarlijk personeel”? ')
 
-if poep and piss == 'yes':
-    print('wow goedzo')
-else: print('echt slecht man')
+if dressuur == '4' or jongleren =='3' or acrobatiek == '3' and diploma == 'MBO-4 ondernemen' and rijbewijs == 'ja' and hoed == 'ja' and man == 'ja' or vrouw == 'ja' and lengte > 150 and gewicht > 90 and certificaat == 'ja':
+    print('U voldoet aan de eisen')
+else:
+    print('U voldoet niet aan de eisen')
