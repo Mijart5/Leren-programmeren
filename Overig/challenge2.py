03f6dffa81ef536c420c5f505ed80ffb4b989f82
Mijart <99072617@mydavinci.nl>
2022-09-12 15:55:05 +0200

    challenge 2

---
+++ Overig/challenge2.py
@@ -0,0 +1,6 @@
+uren = float(input('hoe laat is het in uren? '))
+minuten = float(input('hoe laat is het in minuten? '))
+leftoveruren = 24 - uren
+leftoverminuten = 60 - minuten
+
+print(f'{leftoveruren} uren over en {leftoverminuten} minuten over')
