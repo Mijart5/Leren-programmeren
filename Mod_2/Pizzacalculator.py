f277b4ba5e21ad64f5941ae47b4be3df132d4050
Mijart <99072617@mydavinci.nl>
2022-09-12 13:48:54 +0200

    pizzacalculator update

--- Mod_2/Pizzacalculator.py
+++ Mod_2/Pizzacalculator.py
@@ -1,30 +1,62 @@
 #Mart kooijman's pizzacalculator
+from asyncio.windows_events import NULL
+from contextlib import nullcontext
+from logging import NullHandler
 
-try:
-    klein = int(input('Hoeveel kleine pizzas wilt u? '))
-except:
-    print('Dat is geen nummer.')
-try:
-    medium = int(input('Hoeveel Medium pizzas wilt u? '))
-except:
-    print('Dat is geen nummer')
-try:
-    groot = int(input('Hoeveel grote pizza wilt u? '))
-except:
-    print('Dat is geen nummer')
 
-try:
-    prijs_klein = float(input('Wat is de prijs van de kleine pizzas? '))
-except:
-    print('Dat is geen nummer.')
-try:
-    prijs_medium = float(input('Wat is de prijs van de medium pizzas? '))
-except:
-    print('Dat is geen nummer.')
-try:
-    prijs_groot = float(input('wat is de prijs van de grote pizzas? '))
-except:
-    print('Dat is geen nummer.')
+klein = NULL
+while not klein:
+    try:
+        klein = int(input('Hoeveel kleine pizzas wilt u? '))
+        break
+    except:
+        print('Dat is geen nummer, probeer opnieuw.')
+        continue
+
+medium = NULL
+while not medium:
+    try:
+        medium = int(input('Hoeveel Medium pizzas wilt u? '))
+        break
+    except:
+        print('Dat is geen nummer, probeer opnieuw.')
+        continue
+
+groot = NULL
+while not groot:
+    try:
+        groot = int(input('Hoeveel grote pizza wilt u? '))
+        break
+    except:
+        print('Dat is geen nummer, probeer opnieuw.')
+        continue
+
+prijs_klein = NULL
+while not prijs_klein:
+    try:
+        prijs_klein = float(input('Wat is de prijs van de kleine pizzas? '))
+        break
+    except:
+        print('Dat is geen nummer, probeer opnieuw.')
+        continue
+
+prijs_medium = NULL
+while not prijs_medium:
+    try:
+        prijs_medium = float(input('Wat is de prijs van de medium pizzas? '))
+        break
+    except:
+        print('Dat is geen nummer, probeer opnieuw.')
+        continue
+
+prijs_groot = NULL
+while not prijs_groot:
+    try:
+        prijs_groot = float(input('wat is de prijs van de grote pizzas? '))
+        break
+    except:
+        print('Dat is geen nummer, probeer opnieuw.')
+        continue
 
 totaal_prijs = klein * prijs_klein + medium * prijs_medium + groot * prijs_groot
 
