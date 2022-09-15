cc42de0d8411f8c5eeb906dc62282e51b604ce14
Mijart <99072617@mydavinci.nl>
2022-09-15 14:31:57 +0200

    patches

--- Mod_3/Robotarm/robotarm-python-2021-main/robotarm-python-2021-main/exercise6.py
+++ Mod_3/Robotarm/robotarm-python-2021-main/robotarm-python-2021-main/exercise6.py
@@ -2,13 +2,6 @@ from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 6')
 
-# Jouw python instructies zet je vanaf hier:
-
-
-
-
-
-# Na jouw code wachten tot het sluiten van de window:
 robotArm.grab()
 for a in range(0,6):
     robotArm.moveRight()
