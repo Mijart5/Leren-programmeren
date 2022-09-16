4c0ccab4122076c4319e58eccf9e078fc543954a
Mijart <99072617@mydavinci.nl>
2022-09-16 11:04:09 +0200

    dagen tot dan added

---
+++ Mod_3/dagen tot dan.py
@@ -0,0 +1,26 @@
+from asyncio.windows_events import NULL
+
+
+dag = input('Welke dag is het vandaag? ')
+while dag <= dag:
+    print('Maandag')
+    if dag == 'maandag':
+        break
+    print('Dinsdag')
+    if dag == 'dinsdag':
+        break
+    print('Woensdag')
+    if dag == 'woensdag':
+        break
+    print('Donderdag')
+    if dag == 'donderdag':
+        break
+    print('Vrijdag')
+    if dag == 'vrijdag':
+        break
+    print('Zaterdag')
+    if dag == 'zaterdag':
+        break
+    print('Zondag')
+    if dag == 'zondag':
+        break
