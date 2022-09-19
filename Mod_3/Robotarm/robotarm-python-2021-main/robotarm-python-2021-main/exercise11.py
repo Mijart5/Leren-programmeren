3aae4ca2e29807f41de26a646f957a93cf49b747
Mijart <99072617@mydavinci.nl>
2022-09-19 14:47:40 +0200

    robotarm 11

--- Mod_3/Robotarm/robotarm-python-2021-main/robotarm-python-2021-main/exercise11.py
+++ Mod_3/Robotarm/robotarm-python-2021-main/robotarm-python-2021-main/exercise11.py
@@ -1,3 +1,13 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 11')
+for i in range(9):
+    robotArm.grab()
+    robotArm.scan()
+    if robotArm.scan() == 'white':
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveRight()
+    else:
+        robotArm.drop()
+        robotArm.moveRight()
