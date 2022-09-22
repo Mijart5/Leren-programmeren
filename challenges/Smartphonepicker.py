a2b9f5edfe9806b34e5f776bcea7a45a0dadc176
Mijart <99072617@mydavinci.nl>
2022-09-22 09:54:50 +0200

    Smartphone picker uitbereiding 1

--- challenges/Smartphonepicker.py
+++ challenges/Smartphonepicker.py
@@ -12,6 +12,10 @@ elif Galaxy22 > iphone13:
     print(f'De Samsung Galaxy S22 is het duurst, de telefoon kost: {Galaxy22:.2f} euro.')
     print(f'De iphone 13 is het goedkoopst, de telefoon kost: {iphone13:.2f} euro.')
     print(f'Het advies is dus om de iphone 13 te kopen. Deze is namelijk {Galaxy22 - iphone13:.2f} goedkoper.')
+elif Galaxy22 > 900 and iphone13 > 900:
+    print(f'De Iphone 13 kost {iphone13:.2f} euro.')
+    print(f'De Samsung Galaxy S22 kost {Galaxy22:.2f} euro.')
+    print('Ons advies is om geen telefoon te kopen, ze zijn te duur')
 elif iphone13 == Galaxy22:
     print(f'De Iphone 13 kost: {iphone13:.2f}')
     print(f'De Samsung Galaxy S22 kost: {Galaxy22:.2f}')
