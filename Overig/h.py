b1e8ca7e355020569c20aa75fa2e7418448e9edb
Mijart <99072617@mydavinci.nl>
2022-09-22 09:39:36 +0200

    smartphone picker added

---
+++ Overig/h.py
@@ -0,0 +1,5 @@
+i = 0
+for i in range(10000):
+    print(i + 1)
+    if i == 20:
+        break
---
+++ challenges/Smartphonepicker.py
@@ -0,0 +1,18 @@
+from telnetlib import GA
+
+
+iphone13 = float(input('Hoe duur is de iPhone 13? '))
+Galaxy22 = float(input('Hoe duur is de Galaxy 22? '))
+
+if iphone13 > Galaxy22:
+    print(f'De iphone 13 is het duurst, de telefoon kost: {iphone13:.2f} euro.')
+    print(f'De Samsung Galaxy S22 is het goedkoopst, de telefoon kost: {Galaxy22:.2f} euro.')
+    print(f'Het advies is dus om de Samsung Galaxy S22 te kopen. Deze is namelijk {iphone13 - Galaxy22:.2f} goedkoper.')
+elif Galaxy22 > iphone13:
+    print(f'De Samsung Galaxy S22 is het duurst, de telefoon kost: {Galaxy22:.2f} euro.')
+    print(f'De iphone 13 is het goedkoopst, de telefoon kost: {iphone13:.2f} euro.')
+    print(f'Het advies is dus om de iphone 13 te kopen. Deze is namelijk {Galaxy22 - iphone13:.2f} goedkoper.')
+elif iphone13 == Galaxy22:
+    print(f'De Iphone 13 kost: {iphone13:.2f}')
+    print(f'De Samsung Galaxy S22 kost: {Galaxy22:.2f}')
+    print('De prijzen zijn dus gelijk, ons advies is om de telefoon te kopen die u zelf wilt.')
