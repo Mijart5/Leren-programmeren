5a615fe76635d038674fa330b288a42c7a207d9e
Mijart <99072617@mydavinci.nl>
2022-09-26 14:54:29 +0200

    getalenrader

--- Mod_3/Getalrader.py
+++ Mod_3/Getalrader.py
@@ -1,19 +1,21 @@
 import random
 spellen = 0
-rondes = 0
+score = 0
 while spellen >= 0:
     num = random.randint(0,1000)
     print(num)
+    rondes = 0
     while rondes >= 0:
         gok = -1
         while gok == -1:
             try:
-                gok = int(input("Voer een getal in: "))
+                gok = int(input('Voer een getal in: '))
             except ValueError:
-                print("Dat is geen getal!")
+                print('Dat is geen getal!')
         total = abs(num - gok)
         if gok == num:
             print('Wow! je hebt het nummer geraden')
+            score += 1
             break
         if total <= 20:
             print('je bent heel warm')
@@ -24,12 +26,17 @@ while spellen >= 0:
         rondes += 1
         print(f'{rondes} Rondes gespeeld')
         if rondes == 10:
-            rondes -= 10
             break
     spellen += 1
     print(f'{spellen} spellen gespeeld')
-    wiljestoppen = input('Wil je stoppen? (ja/nee): ')
-    if wiljestoppen == 'ja':
-        break
+    door = input('Wil je doorgaan? (ja/nee) ')
+    if door == 'ja':
+        rondes = 0
+        print(f'Je score tot nu toe is {score} getalen geraden over {spellen} spellen')
+        continue
+    elif door == 'nee':
+        print(f'je score is {score} getalen geraden over {spellen} spel(len)')
+        raise NameError('Je bent gestopt')
     if spellen == 20: 
+        print(f'je score is {score} getalen geraden over {spellen} spellen')
         break
