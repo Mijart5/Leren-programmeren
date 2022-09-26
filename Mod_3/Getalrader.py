74c30e959acbc68a70e0609188848a467928bcd4
Mijart <99072617@mydavinci.nl>
2022-09-26 10:17:18 +0200

    getal rader begin

--- Mod_3/Getalrader.py
+++ Mod_3/Getalrader.py
@@ -1,17 +1,18 @@
 import random
 spellen = 0
 rondes = 0
-num = random.randint(1,200)
+num = random.randint(0,1000)
+print(num)
 while spellen >= 0:
     while rondes >= 0:
         gok = int(input('Raad een nummer! '))
         if gok == num:
             print('Wow! je hebt het nummer geraden')
             break
-        elif num - gok >= 50:
-            print('je bent warm')
-        elif num - gok >= 20:
+        if num - gok <= 20:
             print('je bent heel warm')
+        elif num - gok <= 50:
+            print('je bent warm')
         else:
             print('Koud raad nog eens!')
         rondes += 1
