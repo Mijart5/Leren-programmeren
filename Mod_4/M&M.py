17be8f67a654721788fef0602f9ba1f436df346f
Mijart <99072617@mydavinci.nl>
2022-09-30 10:29:10 +0200

    M&ms

--- Mod_4/M&M.py
+++ Mod_4/M&M.py
@@ -1,1 +1,16 @@
-#M&M
+import random 
+
+MMtuple = ("oranje", "blauw", "groen", "bruin")
+
+aantal_mm = int(input("hoeveel M&Ms worden toegevoegd aan de zak?: "))
+
+A_oranje = random.randint(0,aantal_mm)
+print(f"aantal oranje mms in de zak = {A_oranje}")
+na_oranje = aantal_mm - A_oranje
+
+A_blauw = random.randint(0,na_oranje)
+print(f"aantal blauwe mms in de zak = {A_blauw}")
+na_blauw = na_oranje - A_blauw
+
+A_groen = aantal_mm - A_oranje - A_blauw
+print(f"aantal groene mms in de zak = {A_groen}")
