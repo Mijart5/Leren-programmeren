0c416e27dd622f9c4a96c2e86593dc17c5b22a96
Mijart <99072617@mydavinci.nl>
2022-10-13 10:18:50 +0200

    zwembad prog

--- challenges/zwembad.py
+++ challenges/zwembad.py
@@ -1,6 +1,38 @@
-l = 8
-b = 3
-d = 1.5
-inhoud = l * b * d
+lengte = int(input("Wat is de lengte van uw zwembad?: "))
+breedte = int(input("Wat is de breedte van uw zwembad?: "))
+diepte = int(input("Wat is de diepte van uw zwembad?: "))
+Grootte_zwembad = lengte * breedte * diepte
 
-print(f'{inhoud:.2f}')
+print(f'{Grootte_zwembad:.2f} M3')
+
+uitgraaf_kosten = 25
+uitgraaf_kosten = Grootte_zwembad * uitgraaf_kosten
+
+afvoer_kosten = 32.50
+afvoer_kosten = Grootte_zwembad * afvoer_kosten
+
+totaal_graven = uitgraaf_kosten + afvoer_kosten
+
+afstand = 60
+if Grootte_zwembad < 20:
+    voorrijkosten = 100
+    if afstand < 50:
+        voorrijkosten = voorrijkosten + afstand * 1.25
+    elif afstand > 50:
+        voorrijkosten = voorrijkosten +afstand * 1.15
+else:
+    voorrijkosten = 250
+    if afstand < 50:
+        voorrijkosten = voorrijkosten + afstand * 2.15
+    elif afstand > 50:
+        voorrijkosten = voorrijkosten + afstand * 2.05
+
+
+
+print(f'''
+Uitgraven: $ {uitgraaf_kosten:.2f}
+Afvoeren: $ {afvoer_kosten:.2f}
+Totaal: $ {totaal_graven:.2f}
+Voorrijkosten: $ {voorrijkosten:.2f}
+'''
+)
