8e96dd653466a063701948cd7970eab3743071b8
Mijart <99072617@mydavinci.nl>
2022-10-14 14:03:57 +0200

    wow

--- Mod_4/M&M.py
+++ Mod_4/M&M.py
@@ -1,9 +1,22 @@
+import random
+
 geheel = int(input("Hoeveel m&ms moeten in de zak? (EVEN GETAL): "))
-deelr = geheel // 4
-deelg = geheel // 4
-deelb = geheel // 4
-deelbr = geheel // 4
-bag = (f"rood {deelr}", f"groen {deelg}", f"blauw {deelb}", f"bruin {deelb}")
+
+A_rood = random.randint(0,geheel)
+na_rood = geheel - A_rood
+
+A_blauw = random.randint(0,na_rood)
+na_blauw = na_rood - A_blauw
+
+A_groen = random.randint(0,na_blauw)
+na_groen = na_blauw - A_groen
+
+A_geel = random.randint(0,na_groen)
+na_geel = na_groen - A_geel
+
+A_bruin = na_geel
+
+bag = (f"rood {A_rood}", f"groen {A_groen}", f"blauw {A_blauw}", f"bruin {A_bruin}")
 zak = []
 
 for i in range(4):
