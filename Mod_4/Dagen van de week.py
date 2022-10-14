a01db402f8cba9bf2b9679bb1874be69af41061d
Mijart <99072617@mydavinci.nl>
2022-10-14 12:26:19 +0200

    dagen van de week

--- Mod_4/Dagen van de week.py
+++ Mod_4/Dagen van de week.py
@@ -6,3 +6,17 @@ for i in range(5):
 print("---------------")
 print(week[5])
 print(week[6])
+print("---------------")
+
+for i in range(6, 0, -1):
+    print(week[i])
+    i =- 1
+print(week[0])
+print("---------------")
+
+for i in range(4, 0 ,-1):
+    print(week[i])
+print(week[0])
+print("---------------")
+print(week[6])
+print(week[5])
