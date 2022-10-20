88e7b0499ab62e9507198924b034a0bc9ae7fdde
Mijart <99072617@mydavinci.nl>
2022-10-20 13:04:13 +0200

    mod 4 fin

--- Mod_4/M&M.py
+++ Mod_4/M&M.py
@@ -1,27 +1,15 @@
 import random
 
-kleuren = ("Rood", "Groen", "Blauw", "Bruin")
-aantal = int(input("Hoeveel M&Ms moeten toegevoegd worden aan de zak?: "))
-zak = []
+MAX_RANDOM = 2
 
-deel = random.randint(1,aantal)
-ndeel = aantal - deel
+kleuren = ["Rood", "Blauw", "Groen", "Geel", "Bruin"]
+results = []
+amount = int(input("Hoeveel M&ms moeten in de zak?: "))
 
-deel2 = random.randint(1,ndeel)
-ndeel2 = ndeel - deel2
-
-deel3 = random.randint(1,ndeel2)
-ndeel3 = ndeel2 - deel3
-
-deel4 = random.randint(1,ndeel3)
-ndeel4 = ndeel3 - deel4
-
-A_bruin = ndeel4
-
-print(f"{deel} {deel2} {deel3} {deel4} {deel + deel2 + deel3 + deel4}")
-
-zak.append({kleuren[0] : deel})
-zak.append({kleuren[1] : deel2})
-zak.append({kleuren[2] : deel3})
-zak.append({kleuren[3] : deel4})
-print(zak)
+while amount != 0:
+    num = random.randint(0, min(amount, MAX_RANDOM))
+    color = random.choice(kleuren)
+    for I in range(num):
+      results.append(color)
+    amount -= num
+print(sorted(results))
--- Mod_4/cards.py
+++ Mod_4/cards.py
@@ -1,12 +1,17 @@
 import random
-
-deck = ["Harten 1", "Harten 2", "Harten 3", "Harten 4", "Harten 5", "Harten 6", "Harten 7", "Harten 8", "Harten 9", "Harten 10","Harten Boer", "Harten Vrouw", "Harten Koning", "Harten Aas","Ruiten 1", "Ruiten 2", "Ruiten 3", "Ruiten 4", "Ruiten 5", "Ruiten 6", "Ruiten 7", "Ruiten 8", "Ruiten 9", "Ruiten 10","Ruiten Boer", "Ruiten Vrouw", "Ruiten Koning", "Ruiten Aas","Schoppen 1", "Schoppen 2", "Schoppen 3", "Schoppen 4", "Schoppen 5", "Schoppen 6", "Schoppen 7", "Schoppen 8", "Schoppen 9", "Schoppen 10","Schoppen Boer", "Schoppen Vrouw", "Schoppen Koning", "Schoppen Aas","Klaveren 1", "Klaveren 2", "Klaveren 3", "Klaveren 4", "Klaveren 5", "Klaveren 6", "Klaveren 7", "Klaveren 8", "Klaveren 9", "Klaveren 10","Klaveren Boer", "Klaveren Vrouw", "Klaveren Koning", "Klaveren Aas", "Joker", "Joker"]
-
+deck = []
+spec = ("Koning", "Vrouw", "Boer", "Aas")
+types = ("Harten", "Ruiten", "Schoppen", "Klaver")
+for i in range(4):
+    deck.append({types[0] : spec[i]})
+    deck.append({types[1] : spec[i]})
+    deck.append({types[2] : spec[i]})
+    deck.append({types[3] : spec[i]})
+    for y in range(2,11):
+        deck.append({types[i]: {y}})
 for i in range(7):
     kaart = random.choice(deck)
     print(f"kaart {i + 1} = {kaart}")
-    deck.remove(kaart)
-    
+    deck.remove(kaart) 
 random.shuffle(deck)
-print(" ")
-print(f"deck (49 kaarten): {deck}")
+print(f"\ndeck 47 kaarten: \n{deck}")
