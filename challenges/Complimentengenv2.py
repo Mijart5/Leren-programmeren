91fce3ebe2ac89d7a09dc49f2fc8393540495860
Mijart <99072617@mydavinci.nl>
2022-10-31 14:44:22 +0100

    O6

--- challenges/Complimentengenv2.py
+++ challenges/Complimentengenv2.py
@@ -1,6 +1,23 @@
 from random import randint
 
 naam = input("Wat is uw naam?: ")
+vraag = input("Complimenten of beledigingen?: ")
+aantal = int(input("Hoeveel?: "))
+complimenten = ("Je bent geweldig!", "Goed gewerkt!", "Sick skills!", "Zo slim!")
+beledigingen = ("Je bent kut", "je bent lelijk", "je eet poep voor ontbijt", "git gud", "je bent dik")
+lcijfer = 0
 
-for i in range(randint(1,999)):
-    print(f"Je bent geweldig {naam} ")
+if vraag == "complimenten":
+    for i in range(aantal):
+        cijfer = randint(0,3)
+        while cijfer == lcijfer:
+            cijfer = randint(0,3)
+        print(f"{complimenten[cijfer]}, {naam}")
+        lcijfer = cijfer
+else:
+    for i in range(aantal):
+        cijfer = randint(0,4)
+        while cijfer == lcijfer:
+            cijfer = randint(0,4)
+        print(f"{beledigingen[cijfer]}, {naam}")
+        lcijfer = cijfer 
