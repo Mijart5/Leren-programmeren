565d8a96ca77599ed37c870a145c9cb1b795b4dd
Mijart <99072617@mydavinci.nl>
2023-01-09 15:41:13 +0100

    h

--- Mod_4/Lootjesverdeler.py
+++ Mod_4/Lootjesverdeler.py
@@ -1,21 +1,23 @@
 import random
 door = True
 eerste_list = []
-verdeling = {}
-
 
 while door == True:
     naam = input("vul een naam in: ")
     eerste_list.append(naam)
     if input("wil je de lootjes verdelen? ") == "ja":
-        if len(eerste_list) % 2 != 0:
+        if len(eerste_list) > 3:
             break
         else: 
             print("niet genoeg namen ingevuld")
             continue
-
 tweede_list = eerste_list.copy()
 random.shuffle(tweede_list)
-for i in range(len(eerste_list)):
-    verdeling.update({eerste_list[i] : tweede_list[i]})
-print(verdeling)
+aantal_keer = 0
+while aantal_keer != len(eerste_list):
+    for i in range(len(eerste_list)):
+        if eerste_list[i] == tweede_list[i]:
+            random.shuffle(tweede_list)
+        else:
+            print(eerste_list[i], tweede_list[i])
+            aantal_keer += 1
