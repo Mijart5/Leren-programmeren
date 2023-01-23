34bfb441eeec9828c5c36646614c0e91c482153d
Mijart <99072617@mydavinci.nl>
2023-01-23 11:57:08 +0100

    golden ratio

---
+++ Mod_5/fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(num1, num2, hoevaak):
+    vol_fibo = []
+    vol_fibo.append(num1)
+    vol_fibo.append(num2)
+    for items in range(1,hoevaak - 1):
+        laatste_nu = vol_fibo[items -1] + vol_fibo[items] 
+        vol_fibo.append(laatste_nu)
+    gulden_snede = vol_fibo[len(vol_fibo)-2] / vol_fibo[len(vol_fibo)-1]
+    return vol_fibo, gulden_snede
+print(fibonacci(0,1,10))
